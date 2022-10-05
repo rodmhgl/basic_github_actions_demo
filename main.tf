73c84bb34be2bb1d33705fefcd7b997fe5d415ad
Rod Stewart <rodstewart@gmail.com>
2022-10-04 19:38:32 -0500

    Playing with azure auth

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ terraform {
   }
 }
 
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+
 provider "azurerm" {
   features {}
 
