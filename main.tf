00667ce1c99b9be405fb1a1e1a8733ff312398ad
Rod Stewart <rodstewart@gmail.com>
2022-10-04 19:36:36 -0500

    Playing with azure auth

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
 }
 
 data "azurerm_client_config" "current" {}
