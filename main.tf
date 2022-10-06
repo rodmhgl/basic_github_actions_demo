667a14660574550053c1218c3cb14debe4522be7
Rod Stewart <rodstewart@gmail.com>
2022-10-06 10:15:37 -0500

    Pins Terraform version to 1.3.0

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "=1.3.0"
   backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "Rod-training"
@@ -9,18 +10,8 @@ terraform {
   }
 }
 
-/* variable "ARM_SUBSCRIPTION_ID" {}
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
-variable "ARM_TENANT_ID" {} */
-
 provider "azurerm" {
   features {}
-
-  /* subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID */
 }
 
 data "azurerm_client_config" "current" {}
